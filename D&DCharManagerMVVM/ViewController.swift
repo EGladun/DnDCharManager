a2e83e8776badae16a1e613e88b773d8386ebe86
EGladun <gladunov.e.i.1.17@gmail.com>
2021-05-08 15:06:09 +0300

    Initial Commit

---
+++ D&DCharManagerMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  D&DCharManagerMVVM
+//
+//  Created by Egor on 08.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
