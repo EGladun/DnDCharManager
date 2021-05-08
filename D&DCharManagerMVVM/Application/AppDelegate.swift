35e4b0582d942abc97e16f45ff0fa6154e7b44b1
EGladun <gladunov.e.i.1.17@gmail.com>
2021-05-08 15:14:06 +0300

    Add folder structure

---
+++ D&DCharManagerMVVM/Application/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  D&DCharManagerMVVM
+//
+//  Created by Egor on 08.05.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
